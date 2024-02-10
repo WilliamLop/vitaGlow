3c88e9a6fc3096016d34b8d9a17c8a3b52faba87
William <calderonwilliam25@gmail.com>
2024-02-10 14:21:12 -0500

    🎈

--- components/BtnWhatsApp.jsx
+++ components/BtnWhatsApp.jsx
@@ -7,7 +7,7 @@ const BtnWhatsApp = () => {
     return (
         <>
             <div className='fixed bottom-6 right-6 z-20'>
-                <Link href="https://api.whatsapp.com/send?phone=3202494811" target='_blank'>
+                <Link href="https://api.whatsapp.com/send?phone=573202494811" target='_blank'>
                     <Image src={iconWhat} width={60} height={60} className='shadow-lg rounded-full border-4
                     hover:border-color hover:border-neutral-900/80 duration-300 ease'/>
                 </Link>
